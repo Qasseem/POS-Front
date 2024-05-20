341d0e976da14901bfb6461f3d3487320222e9eb
Qassem <qasem@afaqy.com>
2024-05-20 18:09:51 +0300

    work on merchant details

--- src/app/modules/merchant/pages/view-merchant/view-merchant.module.ts
+++ src/app/modules/merchant/pages/view-merchant/view-merchant.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ViewMerchantComponent } from './view-merchant.component';
-
+import { RouterModule, Routes } from '@angular/router';
+import { PrimeNgButtonsModule } from 'src/app/core/shared/primeng/primeng-button-module';
+import { AppTableModule } from 'src/app/core/shared/core/modules/table/table-module';
+const routes: Routes = [
+  {
+    path: '',
+    component: ViewMerchantComponent,
+  },
+];
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    AppTableModule,
+    PrimeNgButtonsModule,
   ],
-  declarations: [ViewMerchantComponent]
+  declarations: [ViewMerchantComponent],
 })
-export class ViewMerchantModule { }
+export class ViewMerchantModule {}
