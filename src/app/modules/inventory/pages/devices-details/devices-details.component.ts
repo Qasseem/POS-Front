0cd13d56a9fe4c05c7884aa0e4532bf07e435843
Qassem <qasem@afaqy.com>
2025-02-15 23:31:23 +0200

    word on device module

--- src/app/modules/inventory/pages/devices-details/devices-details.component.ts
+++ src/app/modules/inventory/pages/devices-details/devices-details.component.ts
@@ -11,6 +11,9 @@ import { take } from 'rxjs';
 export class DevicesDetailsComponent implements OnInit {
   id;
   details;
+  cards = [];
+  detailsSections: { type: string; label: string; value: any }[][];
+
   constructor(
     private service: DevicesService,
     private route: ActivatedRoute,
@@ -30,8 +33,77 @@ export class DevicesDetailsComponent implements OnInit {
       .subscribe((resp) => {
         if (resp.success) {
           this.details = resp.data;
+          this.prepareCardsData();
+          this.prepareFiledsData();
         }
       });
+  }
+  prepareFiledsData() {
+    this.detailsSections = [
+      [
+        { type: '', label: 'Device ID', value: this.details?.id },
+        { type: '', label: 'Device Type', value: this.details?.deviceType },
+        { type: '', label: 'IMEI', value: this.details?.imei },
+        { type: '', label: 'Device Serial', value: this.details?.serialNumber },
+        { type: '', label: 'SIM Serial', value: this.details?.simCardSerial },
+        { type: '', label: 'Status', value: this.details?.statusName },
+        {
+          type: '',
+          label: 'Errand Channel',
+          value: this.details?.errandChannel,
+        },
+        { type: '', label: 'Warehouse', value: this.details?.warehouse },
+      ],
+      [
+        { type: '', label: 'Merchant Name', value: this.details?.merchantName },
+        { type: '', label: 'Merchant ID', value: this.details?.merchantId },
+        { type: '', label: 'Terminal ID', value: this.details?.terminalId },
+        { type: '', label: 'Ticket ID', value: this.details?.ticketId },
+        { type: '', label: 'Region', value: this.details?.region },
+        { type: '', label: 'City', value: this.details?.city },
+        { type: '', label: 'Zone', value: this.details?.zone },
+        { type: '', label: 'Address', value: this.details?.address },
+      ],
+      [
+        { type: '', label: 'Model Category', value: this.details?.category },
+        { type: '', label: 'Shipment ID', value: this.details?.shipmentId },
+        {
+          type: '',
+          label: 'Device Condition',
+          value: this.details?.conditionName,
+        },
+        { type: 'date', label: 'Created At', value: this.details?.createDate },
+        {
+          type: 'date',
+          label: 'Installed At',
+          value: this.details?.installedAt,
+        },
+      ],
+    ];
+  }
+  prepareCardsData() {
+    this.cards.push({
+      title: 'Owner',
+      name: this.details?.agent,
+      phone: this.details?.agentPhone,
+    });
+    this.details?.warehouseManagers?.forEach((user) => {
+      this.cards.push({
+        title: 'Warehouse Manager',
+        name: user.warehouseManager,
+        phone: user.warehouseManagerPhone,
+      });
+    });
+    this.cards.push({
+      title: 'Created By',
+      name: this.details?.createdBy,
+      phone: this.details?.createdByPhone,
+    });
+    this.cards.push({
+      title: 'Deployment Agent',
+      name: this.details?.deploymentAgent,
+      phone: this.details?.deploymentAgentPhone,
+    });
   }
   backToList() {
     this.router.navigate(['main/inventory/devices/list']);
